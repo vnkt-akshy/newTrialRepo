9ffe6cb55a20fa497d67722f7b95e89ef7b2c4b8
Akshay Venkat <akshay.vnkt@outlook.com>
2021-08-30 15:56:03 +0530

    frstCommit

---
+++ js/js-coding.js
@@ -0,0 +1,12 @@
+var name = "Abc";
+var age = 26;
+console.log("The name is : " + name + " and the age is : " + age);
+
+var val1 = "23";
+var val2 = 23.0;
+
+if(val1 == val2){
+    console.log("Same");
+} else{
+    console.log("Nope");
+}
